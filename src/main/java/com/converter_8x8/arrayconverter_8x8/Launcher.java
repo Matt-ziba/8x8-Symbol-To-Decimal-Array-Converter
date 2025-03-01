6077ad2bee8069f7777b91190ec819fea70272eb
motul <motulok08@gmail.com>
2025-03-01 12:16:17 +0100

    Finished this piece of shit

---
+++ src/main/java/com/converter_8x8/arrayconverter_8x8/Launcher.java
@@ -0,0 +1,9 @@
+package com.converter_8x8.arrayconverter_8x8;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Converter8x8.main(args);
+    }
+}
